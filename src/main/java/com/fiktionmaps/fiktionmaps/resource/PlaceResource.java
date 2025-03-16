ed384912d4ff78808124fcd661eed6043b1f0050
FacundoSZ <fsuarez64arquant.com.ar>
2025-03-16 15:10:48 -0300

    sads

--- src/main/java/com/fiktionmaps/fiktionmaps/resource/PlaceResource.java
+++ src/main/java/com/fiktionmaps/fiktionmaps/resource/PlaceResource.java
@@ -1,19 +1,28 @@
 package com.fiktionmaps.fiktionmaps.resource;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.fiktionmaps.fiktionmaps.mapper.PlaceMapper;
 import com.fiktionmaps.fiktionmaps.model.Place;
 import com.fiktionmaps.fiktionmaps.repository.PlaceRepository;
 import com.fiktionmaps.fiktionmaps.service.PlaceService;
 import com.fiktionmaps.fiktionmaps.service.UserService;
 import com.fiktionmaps.fiktionmaps.service.dto.PlaceDTO;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/places")
